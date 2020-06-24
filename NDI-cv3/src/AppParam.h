48ff8f79df026b3548edd6f98208663cbb526804
Hiroshi Matoba <matobahiroshi@gmail.com>
2020-06-24 10:46:12 +0200

    fix __DATE__

--- NDI-cv3/src/AppParam.h
+++ NDI-cv3/src/AppParam.h
@@ -11,57 +11,17 @@ namespace mtb {
 
 	public:
 
-		AppParam() {
-			targetFps.addListener(this, &AppParam::targetFpsChanged);
-			windowTitle.addListener(this, &AppParam::windowTitleChanged);
-			logLevel.addListener(this, &AppParam::logLevelChanged);
+        AppParam();
+        ~AppParam();
+        void targetFpsChanged(int& fps);
+        void windowTitleChanged(std::string& title);
+        void logLevelChanged(std::string& level);
 
-			//bStart.setSerializable(false);
-			fps.setSerializable(false);
-
-
-			//TODO: Maybe there is a better way of doing this?
-			logLevelMap.insert(std::make_pair("OF_LOG_VERBOSE", OF_LOG_VERBOSE));
-			logLevelMap.insert(std::make_pair("OF_LOG_NOTICE", OF_LOG_NOTICE));
-			logLevelMap.insert(std::make_pair("OF_LOG_WARNING", OF_LOG_WARNING));
-			logLevelMap.insert(std::make_pair("OF_LOG_ERROR", OF_LOG_ERROR));
-			logLevelMap.insert(std::make_pair("OF_LOG_FATAL_ERROR", OF_LOG_FATAL_ERROR));
-			logLevelMap.insert(std::make_pair("OF_LOG_SILENT", OF_LOG_SILENT));
-			ofSetLogLevel(logLevelMap.at(logLevel));
-		}
-
-		~AppParam() {
-			targetFps.removeListener(this, &AppParam::targetFpsChanged);
-		};
-
-		void targetFpsChanged(int& fps) {
-			ofSetFrameRate(fps);
-		}
-
-		void windowTitleChanged(std::string& title) {
-			ofSetWindowTitle(title);
-		}
-
-		void logLevelChanged(std::string& level) {
-			ofSetLogLevel(logLevelMap.at(level));
-		}
-
-		void set() {
-			fps = ofGetFrameRate();
-
-			// this makes app slower
-			//bSmoothing ? ofEnableSmoothing() : ofDisableSmoothing();
-
-			bAlphaBlending ? ofEnableAlphaBlending() : ofDisableAlphaBlending();
-			bAntiAliasing ? ofEnableAntiAliasing() : ofDisableAntiAliasing();
-			bDepthTest ? ofEnableDepthTest() : ofDisableDepthTest();
-			ofSetVerticalSync(bVerticalSync);
-			ofBackground(bg);
-		}
+        void set();
 
 		std::map<std::string, ofLogLevel> logLevelMap;
 
-		ofParameter<std::string> appVersion{ "App Version", __DATE__ };
+		ofParameter<std::string> appVersion{ "App Version"};
 		ofParameter<std::string> windowTitle{ "Window Title", "" };
 		ofParameter<std::string> logLevel{ "Log Level", "OF_LOG_SILENT" };
         //ofParameter<bool> bStart{ "Start", false };
