dce8aa4763cfba48053727e9d8d4871816662e60
Hiroshi Matoba <matobahiroshi@gmail.com>
2020-10-27 14:07:21 +0100

    remove 10 min heatmap reset

--- NDI-cv5/src/NDIsource.h
+++ NDI-cv5/src/NDIsource.h
@@ -139,16 +139,6 @@ public:
 
 			drawFbo();
 		}
-
-		// ofxHeatmap increase memory use, so we reset every 10 min.
-		if (bHeatmap) {
-			int resetFrame = ofGetTargetFrameRate() / 2 * 60 * 10;
-			if ( (ofGetFrameNum() % resetFrame) == (resetFrame-1)) {
-				heatmap.clear();
-				heatmap.setup(processWidth, processHeight);
-				cout << "clear heatmap" << endl;
-			}
-		}
     }       
   
     void findContour(){
